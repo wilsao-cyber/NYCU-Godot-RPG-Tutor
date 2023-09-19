8b40bf0510d744e352071ded5c9937294b3b1231
wilsao-cyber <72856265+wilsao-cyber@users.noreply.github.com>
2023-09-20 05:42:30 +0800

    enum update

---
+++ BattleWidgets/battle_scene.gd
@@ -0,0 +1,44 @@
+extends Control
+
+@export var CurBattle := BattleLogic
+@export var CurSys := SystemLogic
+enum BattleLogic{
+	PlayerTurn,
+	EnemyTurn,
+	SystemTurn
+}
+
+enum SystemLogic{
+	StartBattle,
+	PlayerDied,
+	EnemyDied,
+	EndBattle
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+func BattleStatus():
+	match CurBattle:
+		BattleLogic.PlayerTurn:
+			pass
+		BattleLogic.EnemyTurn:
+			pass
+		BattleLogic.SystemTurn:
+			pass
+	pass
+	
+func SystemStatus():
+	match CurSys:
+		SystemLogic.StartBattle:
+			pass
+		SystemLogic.PlayerDied:
+			pass
+		SystemLogic.EnemyDied:
+			pass
+		SystemLogic.EndBattle:
+			pass
+		
+	pass
